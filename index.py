fff19694f2c285572607ce8e85c74f14eb50d2a8
rajanvalencia <rajan.valencia@au.com>
2021-08-17 05:59:59 +0900

    first commit

---
+++ index.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from decouple import config
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import NoSuchElementException
+import time 
+
+def main():
+    driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+    driver.get("https://instagram.com")
+    time.sleep(2)
+
+    username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+    username.send_keys(config('USERNAME'))
+    password.send_keys(config('PASSWORD'))
+
+    submit = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+    time.sleep(5)
+    later_button = EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), '後で')]"))
+    WebDriverWait(driver, 10).until(later_button).click()
+    WebDriverWait(driver, 10).until(later_button).click()
+
+    total_likes = 0;
+
+    hashtags = ["多言語", "多文化", "多文化共生", "多文化教育", "多文化交流",   "多様性", "日本語", "日本語学校", "日本語教師", "日本語勉強"]
+
+    for hashtag in hashtags:
+        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+        time.sleep(5)
+
+        for i in range(3):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+
+        links = driver.find_elements_by_tag_name("a")
+        links = [l.get_attribute("href") for l in links]
+        links = [l for l in links if l.startswith("https://www.instagram.com/p/")]
+
+        for link in links[:40]:
+            try:
+                driver.get(link)
+
+                try:
+                    like_button = driver.find_element_by_xpath("//*[@aria-label='いいね！']")
+                    like_button.click()
+                    total_likes +=  1
+                except NoSuchElementException:
+                    pass
+            
+                # greeting = ["Hi", "Hello", "Hey", "Heeey", "Greetings"]
+                # random_idx = random.randint(0, (len(greeting)-1))
+                # my_comment = greeting[random_idx] + " I saw your photos - they are beautiful! wanna collab??"
+                # form = driver.find_element_by_tag_name("form").click()
+                # text_area = driver.find_element_by_tag_name("textarea")
+                # text_area.send_keys(my_comment)
+                # submit = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+            except WebDriverException:
+                print_and_close(driver, total_likes)
+
+    print_and_close(driver, total_likes)
+
+def print_and_close(driver, total_likes):
+    print('Total likes: ', total_likes)
+    time.sleep(2)
+    driver.close()
+
+if __name__ == "__main__":
+    main()
